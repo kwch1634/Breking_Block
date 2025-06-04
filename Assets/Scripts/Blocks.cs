f6c0e73dacc7b1fff38436210fdfe6afcb49bfb3
川鍋春輝 <125I039@stu.nit.ac.jp>
2025-06-04 17:26:38 +0900

    ふ

--- Assets/Scripts/Blocks.cs
+++ Assets/Scripts/Blocks.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 class Blocks : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem particle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnCollisionEnter(Collision collision)
     {
         GameObject.Find("GameManager").GetComponent<GameManager>().OnBlockDestroyed();
+        particle.Play();
         Destroy(gameObject);
     }
 
